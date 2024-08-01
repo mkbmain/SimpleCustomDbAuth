05f429b77169503187179d3a13a549f4fadf5326
mkbmain <michael.bourke@live.co.uk>
2024-08-01 19:55:23 +0100

    test

--- AuthCustomDb/Controllers/SimpleController.cs
+++ AuthCustomDb/Controllers/SimpleController.cs
@@ -12,8 +12,8 @@ public class SimpleController : Controller
 {
     private readonly ITokenGenerator _tokenGenerator;
     private readonly UserDb _userDb;
-    
-    public SimpleController(ITokenGenerator tokenGenerator,UserDb userDb)
+
+    public SimpleController(ITokenGenerator tokenGenerator, UserDb userDb)
     {
         _userDb = userDb;
         _tokenGenerator = tokenGenerator;
@@ -33,22 +33,28 @@ public class SimpleController : Controller
         {
             return BadRequest();
         }
+
         _userDb.Users.Add(new Db.User
-            { Email = email.ToLower(), Password = BCrypt.Net.BCrypt.HashPassword(password), Role="Customer" });
+        {
+            Email = email.ToLower(), Password = BCrypt.Net.BCrypt.HashPassword(password),
+            UserRoles = new List<UserRole>() { new UserRole { RoleId = (int)Roles.Customer } }
+        });
         await _userDb.SaveChangesAsync();
 
         return Created();
     }
 
     [HttpGet]
-    public async Task<IActionResult> Auth([FromQuery] string email,[FromQuery] string password)
+    public async Task<IActionResult> Auth([FromQuery] string email, [FromQuery] string password)
     {
         var user = await _userDb.Users.FirstOrDefaultAsync(w => w.Email == email.ToLower());
         if (user is null || !BCrypt.Net.BCrypt.Verify(password, user.Password))
         {
             return BadRequest();
         }
-        
-        return Ok(new {Token = _tokenGenerator.BuildToken(new TokenGeneratorClaims(email,Guid.NewGuid(), [user.Role]))});
+
+        var roles = user.UserRoles.Select(w => w.Role.Name).ToArray();
+        return Ok(new
+            { Token = _tokenGenerator.BuildToken(new TokenGeneratorClaims(email, Guid.NewGuid(), roles)) });
     }
 }
---
+++ AuthCustomDb/Migrations/20240801184909_roles.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AuthCustomDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class roles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 35, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRole",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RoleId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRole", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRole_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRole_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Role",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Customer" },
+                    { 2, "Admin" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRole_RoleId",
+                table: "UserRole",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRole_UserId",
+                table: "UserRole",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRole");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
